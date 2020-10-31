e781a5c62f3921d63406e4cb319eb63880805409
Soeren Skoett <soerenskoett@gmail.com>
2020-10-31 10:19:00 +0000

    added price handler - WIP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 import "./style.css";
+import {prices, yearPrices} from './store.js';
 
-console.log('running');
+class PriceSelector {
+
+    constructor(element) {
+
+        self = this;
+
+        this.element = element;
+        this.priceControls = this.element.querySelectorAll('.Price--Selector__price-controls li');
+        this.subscription = this.element.querySelector('.Price--Selector__subscription-price');
+        this.period = this.element.querySelector('.Price--Selector__subscription-period');
+
+        const PAYMENT_PERIODS = {
+            monthly: prices,
+            yearly: yearPrices
+        };
+
+        this.state = {
+            paymentPeriod: PAYMENT_PERIODS['monthly'],
+            selectedPrice: 'priceRange1',
+            priceListener: function(val) {},
+            set price(val) {
+                this.selectedPrice = val;
+                this.priceListener(val);
+            },
+            get price() {
+                return this.selectedPrice;
+            },
+            registerListener: function(listener) {
+                this.priceListener = listener;
+            }
+        };
+
+        this.bindEvents();
+        this.setPrice();
+    }
+
+    bindEvents() {
+        self.priceControls.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                self.state.price = item.dataset.priceRange;
+            })
+        });
+
+        self.state.registerListener(function() {
+            self.setPrice();
+        })
+    }
+
+    setPrice() {
+        self.subscription.innerHTML = prices[self.state.price].standard;
+    }
+
+
+}
+
+
+const wrapper = document.querySelector('.Price--Selector');
+
+new PriceSelector(wrapper);
---
+++ src/store.js
@@ -0,0 +1,95 @@
+export const prices = {
+    priceRange1: {
+        standard: 699,
+        startFee: 0,
+        free: 0,
+        customerScore: 149,
+        shippingScore: 149,
+        customerShippingScore: 269,
+        loyalDiscount: 549
+    },
+    priceRange2: {
+        standard: 999,
+        startFee: 0,
+        free: 0,
+        customerScore: 249,
+        shippingScore: 249,
+        customerShippingScore: 449,
+        loyalDiscount: 749
+    },
+    priceRange3: {
+        standard: 1499,
+        startFee: 0,
+        free: 0,
+        customerScore: 349,
+        shippingScore: 349,
+        customerShippingScore: 629,
+        loyalDiscount: 1.099
+    },
+    priceRange4: {
+        standard: 1999,
+        startFee: 0,
+        free: 0,
+        customerScore: 449,
+        shippingScore: 449,
+        customerShippingScore: 809,
+        loyalDiscount: 1.499
+    },
+    priceRange5: {
+        standard: 2499,
+        startFee: 0,
+        free: 0,
+        customerScore: 549,
+        shippingScore: 549,
+        customerShippingScore: 989,
+        loyalDiscount: 1.849
+    }
+};
+
+export const yearPrices = {
+    priceRange1: {
+        standard: 7.549,
+        startFee: 0,
+        free: 0,
+        customerScore: 149,
+        shippingScore: 149,
+        customerShippingScore: 269,
+        loyalDiscount: 5.929
+    },
+    priceRange2: {
+        standard: 10.789,
+        startFee: 0,
+        free: 0,
+        customerScore: 249,
+        shippingScore: 249,
+        customerShippingScore: 449,
+        loyalDiscount: 8.089
+    },
+    priceRange3: {
+        standard: 16.189,
+        startFee: 0,
+        free: 0,
+        customerScore: 349,
+        shippingScore: 349,
+        customerShippingScore: 629,
+        loyalDiscount: 11.869
+    },
+    priceRange4: {
+        standard: 21.589,
+        startFee: 0,
+        free: 0,
+        customerScore: 449,
+        shippingScore: 449,
+        customerShippingScore: 809,
+        loyalDiscount: 16.189
+    },
+    priceRange5: {
+        standard: 26.989,
+        startFee: 0,
+        free: 0,
+        customerScore: 549,
+        shippingScore: 549,
+        customerShippingScore: 989,
+        loyalDiscount: 19.969
+    }
+};
